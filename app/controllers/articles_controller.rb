3f0d18144d049fe78c9569a937653d48a9152797
Sujata <bhattaraisuzata@gmail.com>
2018-01-02 17:43:39 -0600

    modified articles' controller, views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,18 @@ class ArticlesController < ApplicationController
   def create
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+      #do sth
+    else
+      render 'new'
+    end
+    #redirect_to articles_path(@article)
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
