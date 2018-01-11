4583a86e54a4b6b3ae0870ca2e96201b78c47c8a
Sujata <bhattaraisuzata@gmail.com>
2018-01-11 10:49:54 -0600

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get 'signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, except: [:new]
 end
