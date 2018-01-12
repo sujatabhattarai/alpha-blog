b05716f0be029e5cd58b1d9ab6b51b0524a63991
Sujata <bhattaraisuzata@gmail.com>
2018-01-12 16:09:21 -0600

    Add sign in upon sign up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,6 @@ class ArticlesController < ApplicationController
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
-      #do sth
     else
       render 'edit'
     end
