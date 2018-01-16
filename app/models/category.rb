fa67a7a5309067c3d6f15b5b3a59ebac5a57349b
Sujata <bhattaraisuzata@gmail.com>
2018-01-15 23:35:53 -0600

    Add article and category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+
   validates :name, presence: true, uniqueness: true,
             length: {maximum: 25, minimum: 3}
 end
