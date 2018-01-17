ea4e3e918b965c12c6059a99001788a201698247
Sujata <bhattaraisuzata@gmail.com>
2018-01-16 22:56:03 -0600

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -59,7 +59,8 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description,
+                                      category_ids: [])
     end
 
     def require_same_user
